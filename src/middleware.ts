4baa4f207dfb520a911e1a34f47876b518510ad7
SplinterSword <sparshj2003@gmail.com>
2026-04-10 10:31:23 +0530

    changed the name of proxy.ts to middleware.ts to make the middleware work

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+  console.log("MIDDLEWARE RUNNING:", req.nextUrl.pathname)
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
