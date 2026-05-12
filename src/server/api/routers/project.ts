575669a732f997cbffd82fa6ba78a4c7937661e3
SplinterSword <sparshj2003@gmail.com>
2026-05-12 21:19:05 +0530

    added space in create endpoint to avoid errors

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -16,7 +16,7 @@ export const projectRouter = createTRPCRouter({
                 id: ctx.user.userId!,
             },
             select: {
-                credits:true
+                credits: true
             }
         })
         
